9f4e26267d5499d241b03ec7900a5f6bb3d9cc1b
dudufr <55206785+dudufr@users.noreply.github.com>
2019-10-28 18:16:00 -0300

    Update atividade lista 01 element.h

--- atividade lista 01 element.h
+++ atividade lista 01 element.h
@@ -1,40 +1,42 @@
 //element.h
 
-typedef struct node {
-	int data;
-	struct node* next;
-}
-  
-int isEmpty (struct node* element){
-	if (element != NULL)
-		return 0;
-	else
-		return 1;
+struct Node {
+  int data;
+  struct Node* next;
+};
+
+int isEmpty (struct Node* element);
+int size (struct Node* element);
+void insertElement (struct Node* element, int newData);
+void printList (struct Node* element);
+
+int isEmpty (struct Node* element){
+  if (element != NULL)
+    return printf("lista não vazia");
+  else 
+    return printf("lista vazia");
 }
 
-int size(struct node* element){
-	int count = 0;
+int size (struct Node* element){
+  int count = 1;
 	while (element->next != NULL){
 		count = count + 1;
-		element = element->next
+		element = element->next;
 	}
 	return count;
 }
 
-void insertElement(struct node* newElement, int newData){
-	struct node* newElement = (struct node*) malloc(sifeof(struct node));
-	if (element->next == NULL){
-	 	element->next = newElement;
-		newElement->data = newData;
-	}
-}
-
-void printList(struct node* element){
+void printList(struct Node* element){
 	while (element->next != NULL){
 		printf ("%d", element->data);
 		element = element->next;
 	}
 }
 
-  return 0;
+void insertElement(struct Node* element, int newData){
+  struct Node* newElement = (struct Node*)malloc(sizeof (struct Node));
+	if (element->next == NULL){
+	 	element->next = newElement;
+		newElement->data = newData;
+	}
 }
